40532a680a0940dbb6de65044d909ad1cc899af7
Kaptinlin <Kaptinlin@gmail.com>
2012-12-05 08:07:39 -0800

    improve slide type responsive

--- src/jquery.tinySlider.js
+++ src/jquery.tinySlider.js
@@ -200,6 +200,11 @@
                         var width = self.$viewport.width();
                         self.$slides.width(width);
                         self.$ul.width(100 * self.$slides.length + '%');
+                        
+                        $(window).on('resize',function(){
+                            var width = self.$viewport.width();
+                            self.$slides.width(width);
+                        });
                     },
                     run: function(data) {
                         if (!self.cycle) {
